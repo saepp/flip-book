e197f3f7d45997d83a917cafc4358c8a0e5b9091
saepp <dsrisk911@gmail.com>
2022-04-27 13:26:17 +0700

    add script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// References to DOM Elements
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const book = document.querySelector("#book");
+
+const paper1 = document.querySelector("#p1");
+const paper2 = document.querySelector("#p2");
+const paper3 = document.querySelector("#p3");
+
+// Business Logic
+let currentLocation = 1;
+let numOfPapers = 3;
+let maxLocation = numOfPapers + 1;
+
+function openBook() {}
+
+function closeBook() {}
+
+function goNextPage() {}
+
+function goPrevPage() {}
