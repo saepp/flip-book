ddb142c921bf51a46e9c0881e46d13f3f5491e1c
saepp <dsrisk911@gmail.com>
2022-04-27 13:33:45 +0700

    add function goNextPage

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
 
+// Event Listener
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNextPage);
+
 // Business Logic
 let currentLocation = 1;
 let numOfPapers = 3;
@@ -16,6 +20,25 @@ function openBook() {}
 
 function closeBook() {}
 
-function goNextPage() {}
+function goNextPage() {
+  if (currentLocation < maxLocation) {
+    switch (currentLocation) {
+      case 1:
+        openBook();
+        paper1.classList.add("flipped");
+        break;
+      case 2:
+        paper2.classList.add("flipped");
+        break;
+      case 3:
+        paper3.classList.add("flipped");
+        closeBook();
+        break;
+      default:
+        throw new Error("unknown state");
+    }
+    currentLocation++;
+  }
+}
 
 function goPrevPage() {}
